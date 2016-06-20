a4d818f705f0c8a1b05bd0a7511bd97032481e83
Moss Green <gufeifei@outlook.com>
2016-06-20 22:21:16 +1200

    add movies controller

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
