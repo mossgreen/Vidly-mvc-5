e7c69cbcbd84ccdb633608079100f81898938c5b
Moss Green <gufeifei@outlook.com>
2016-06-21 23:01:16 +1200

    do not user view data or view bag, best practice is passing view object

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,19 +14,10 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!"  };
 
+        
             return View(movie);
-           // return Content("hello moss");
-           // return HttpNotFound();
-          // return new EmptyResult();
-         // return RedirectToAction("index","home", new {page=1, sortBy = "name"});
         }
 
-
-        [Route("movies/released/{year}/{month:regex(\\d{4})}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
         
     }
 }
