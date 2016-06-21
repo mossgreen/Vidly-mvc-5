2bcb11ce43758caedf336599c0a4f132e2fa0c7b
Moss Green <gufeifei@outlook.com>
2016-06-21 23:19:40 +1200

    tried to manipulate razor view

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -14,8 +15,18 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!"  };
 
-        
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"},
+            };
+            var viewModal = new RandomMovieViewModal
+            {
+                Movie = movie,
+                Customers =  customers,
+            };
+
+            return View(viewModal);
         }
 
         
