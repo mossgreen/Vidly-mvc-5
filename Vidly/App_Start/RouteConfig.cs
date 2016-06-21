b57130a7ad1f618fe098293f130a9b6c230cf739
Moss Green <gufeifei@outlook.com>
2016-06-21 22:51:06 +1200

    tested. tried to build a customer router.

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -12,6 +12,12 @@ namespace Vidly
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new {controller = "Movies", action = "ByReleaseDate"},
+                new {year = @"2015|2016", month=@"\d{2}"}
+                );
 
             routes.MapRoute(
                 name: "Default",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,11 +14,37 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!"  };
 
-            //return View(movie);
+            return View(movie);
            // return Content("hello moss");
            // return HttpNotFound();
           // return new EmptyResult();
-          return RedirectToAction("index","home", new {page=1, sortBy = "name"});
+         // return RedirectToAction("index","home", new {page=1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id =" +id);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        //movies
+        // ? sign here stands for nullable
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
         }
     }
 }
