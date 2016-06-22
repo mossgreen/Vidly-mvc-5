a6c392a37dcd39284ee745e08275505b4f47b316
Moss Green <gufeifei@outlook.com>
2016-06-22 23:23:09 +1200

    PM> add-migration ApplyAnnotationsToCustomerName

--- Vidly/Migrations/201606221115587_PopulateMembershipTypes.cs
+++ Vidly/Migrations/201606221115587_PopulateMembershipTypes.cs
@@ -7,6 +7,10 @@ namespace Vidly.Migrations
     {
         public override void Up()
         {
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(1,0,0,0)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(2,30,1,10)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(3,90,3,15)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES(4,300,12,20)");
         }
         
         public override void Down()
