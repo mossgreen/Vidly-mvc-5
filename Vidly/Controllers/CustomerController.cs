f612981c7eaeb867c81cebab0f99a153c508b11d
Moss Green <gufeifei@outlook.com>
2016-06-22 23:57:28 +1200

    find details with github

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: Customer
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+        public CustomerController()
         {
-            return View();
+            _context = new ApplicationDbContext();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+           _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            var customer = _context.Customers.ToList();
+            return View(customer);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
     }
 }
