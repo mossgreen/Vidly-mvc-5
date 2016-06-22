a526ca442a36791b26bff65f31be69c1091caed5
Moss Green <gufeifei@outlook.com>
2016-06-22 23:14:17 +1200

    Name: AddMembershipType

---
+++ Vidly/Migrations/201606221113548_AddMembershipType.cs
@@ -0,0 +1,38 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.MembershipTypes",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        SignUpFee = c.Short(nullable: false),
+                        DurationInMonths = c.Byte(nullable: false),
+                        DiscountRate = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Customers", "IsSubscribedToNewletter", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Customers", "MembershipTypeId", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "Id", cascadeDelete: true);
+            DropColumn("dbo.Customers", "IsSubscribedToNewsletter");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Customers", "IsSubscribedToNewsletter", c => c.Boolean(nullable: false));
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropColumn("dbo.Customers", "MembershipTypeId");
+            DropColumn("dbo.Customers", "IsSubscribedToNewletter");
+            DropTable("dbo.MembershipTypes");
+        }
+    }
+}
