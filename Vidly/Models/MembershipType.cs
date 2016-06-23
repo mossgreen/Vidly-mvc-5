988e93e6c503c5a7fd935e3c4f1a8f531214557e
Moss Green <gufeifei@outlook.com>
2016-06-24 11:44:12 +1200

    add-migration: AddNameToMembershipType

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace Vidly.Models
 {
     public class MembershipType
     {
-        public byte id { get; set; }
+        public byte Id { get; set; }
+        [Required]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
