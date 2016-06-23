1602e7a166e0beccff6813e962055ff420b54f4d
Moss Green <gufeifei@outlook.com>
2016-06-23 23:24:03 +1200

    PM> add-migration AddMembershipType

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 
 namespace Vidly.Models
@@ -9,5 +10,8 @@ namespace Vidly.Models
     {
         public int  Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
-}
+} 
