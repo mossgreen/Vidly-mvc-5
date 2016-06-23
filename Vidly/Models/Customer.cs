a10cc3aa9aed9525eb1101f23c81df5b2614547f
Moss Green <gufeifei@outlook.com>
2016-06-23 23:36:35 +1200

    PM> add-migration ApplyAnnotationsToCustomerName

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Web;
@@ -9,6 +10,8 @@ namespace Vidly.Models
     public class Customer
     {
         public int  Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
