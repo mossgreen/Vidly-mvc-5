989c0987062a2b158af0ef768581b95e4eedb0d1
Moss Green <gufeifei@outlook.com>
2016-06-24 12:28:13 +1200

    PM> add-migration AddMovie

---
+++ Vidly/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
