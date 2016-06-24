d812445ed3e57425e26dbb4a8065222854df95e1
Moss Green <gufeifei@outlook.com>
2016-06-25 11:22:35 +1200

    client-side validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -23,6 +23,7 @@ namespace Vidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes,
             };
             return View("CustomerForm",viewModel);
