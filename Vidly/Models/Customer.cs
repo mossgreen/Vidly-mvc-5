05e2e7dc89311eb1f9873ba3d34427364bd27278
Moss Green <gufeifei@outlook.com>
2016-06-24 21:49:15 +1200

    label for form
    two there two ways to do so:
    1. change the [dispaly] of the model. needs compile, not magic code.
    2. change it in the view. need to hard code with it.
    I prefer the first method.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -16,6 +16,8 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
     }
 } 
