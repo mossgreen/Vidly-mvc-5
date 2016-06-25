2fd3727d9d5384c2517184418586e80251728525
Moss Green <gufeifei@outlook.com>
2016-06-25 23:03:01 +1200

    error
    "message": "An error has occurred.",
      "exceptionMessage": "The
    'ObjectContent`1' type failed to serialize the response body for content
    type 'application/json; charset=utf-8'.",
      "exceptionType":
    "System.InvalidOperationException",

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,8 +12,15 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
+            //Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+
+            //Dto to domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseTime { get; set; }
+
+        [Range(1,20)]
+        public byte NumberInStock { get; set; }
+    }
+}
