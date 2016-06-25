24e7a5a66302fb7d8787f2a0e84a90b5c3f1a451
Moss Green <gufeifei@outlook.com>
2016-06-25 15:10:21 +1200

    add dto

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [Min18YesrsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
