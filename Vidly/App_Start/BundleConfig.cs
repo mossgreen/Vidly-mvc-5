fda152db705e5dc9dbf0064e48b7e1a40d434ba5
Moss Green <gufeifei@outlook.com>
2016-07-01 16:19:24 +1200

    displaying toast notifications

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace Vidly
                       "~/Scripts/respond.js",
                       "~/Scripts/datatables/jquery.datatables.js",
                       "~/Scripts/datatables/datatables.bootstrap.js",
-                      "~/Scripts/typeahead.bundle.js" ));
+                      "~/Scripts/typeahead.bundle.js" ,
+                      "~/Scripts/toastr.js" ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -31,6 +32,7 @@ namespace Vidly
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
