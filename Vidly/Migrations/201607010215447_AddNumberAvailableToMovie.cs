f485dca902b56ba317d2d8decac20e1ba37b6af1
Moss Green <gufeifei@outlook.com>
2016-07-01 14:17:23 +1200

    PM> update-database

---
+++ Vidly/Migrations/201607010215447_AddNumberAvailableToMovie.cs
@@ -0,0 +1,17 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNumberAvailableToMovie : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE Movies SET NumberAvailable = NumberInStock");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
