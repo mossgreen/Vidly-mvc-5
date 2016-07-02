8630d893c91f90ad458e20f8c30625db7b192a4e
Moss Green <gufeifei@outlook.com>
2016-07-03 10:58:16 +1200

    bug fixed.

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -19,6 +19,8 @@ namespace Vidly.Controllers
 
         public ActionResult About()
         {
+
+            throw new Exception();
             ViewBag.Message = "Your application description page.";
 
             return View();
